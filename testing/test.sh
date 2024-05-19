f50a019514e8b1941875e7576aaf0a577b07488d
joselizagaravito <joselizagaravito@gmail.com>
2024-05-18 23:37:21 -0500

    pruebas en workflow

---
+++ testing/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:80)
+if [ "$response" -eq 200 ]; then
+    echo "HTTP status test passed!"
+else
+    echo "HTTP status test failed!"
+    exit 1
+fi
+content=$(curl -s http://localhost:80)
+if [[ "$content" == *"Bienvenidos al curso de DevSecOps"* ]]; then
+    echo "Content test passed!"
+else
+    echo "Content test failed!"
+    exit 1
+fi
